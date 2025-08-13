6bc0776cf893507ae8d8aea8fc902f56ed9ce321
harizz_l <126933217+harizz-l@users.noreply.github.com>
2025-08-13 10:40:26 +0530

    Delete ems-backend directory

--- ems-backend/src/main/java/com/example/ems_backend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ems_backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
--- ems-backend/src/main/java/com/example/ems_backend/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ems_backend.repository;
-
-import com.example.ems_backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
